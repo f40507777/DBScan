81ab0ab23d48467d7d092f307375f8c1311197a9
finnho <finnho@truetel.com>
2018-07-20 11:08:27 +0800

    Initial Commit

---
+++ DBScan/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  DBScan
+//
+//  Created by Finn on 2018/7/20.
+//  Copyright © 2018年 Finn. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
